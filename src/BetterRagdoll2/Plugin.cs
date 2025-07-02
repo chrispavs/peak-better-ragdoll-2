5e1ad5aae861715a9a76856cc40fe2e985eed946
Chris Pavlopoulos <pavlopouloschris2@gmail.com>
2025-07-02 16:16:03 -0500

    woo: init commit

---
+++ src/BetterRagdoll2/Plugin.cs
@@ -0,0 +1,62 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using UnityEngine.TextCore.Text;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using System;
+using System.Reflection;
+
+namespace BetterRagdoll2;
+
+[BepInAutoPlugin]
+public partial class Plugin : BaseUnityPlugin
+{
+    internal static ManualLogSource Log { get; private set; } = null!;
+
+    private ConfigEntry<float>? forceConfig = null;
+
+    private void Awake()
+    {
+        Log.LogInfo($"Plugin {Name} is loaded!");
+
+        this.forceConfig = base.Config.Bind<float>("Settings", "Ragdoll Force", 322f, "Force applied to the character when triggering ragdoll.");
+    }
+
+    private void Update()
+    {
+        if (!(Character.localCharacter == null) && Application.isFocused && this.forceConfig != null)
+        {
+            if (Input.GetKeyDown(KeyCode.F8))
+            {
+                this.forceConfig.Value -= 10f;
+                Debug.Log(string.Format("Force decreased to {0}", this.forceConfig.Value));
+            }
+            if (Input.GetKeyDown(KeyCode.F9))
+            {
+                this.forceConfig.Value += 10f;
+                Debug.Log(string.Format("Force increased to {0}", this.forceConfig.Value));
+            }
+            if (Input.GetKeyDown(KeyCode.X) || (Gamepad.current != null && Gamepad.current.rightShoulder.wasPressedThisFrame))
+            {
+                Character localCharacter = Character.localCharacter;
+                if (localCharacter.data.fallSeconds > 0f)
+                {
+                    Debug.Log("Already in ragdoll – skipping.");
+                    return;
+                }
+                Debug.Log("Ragdoll Key pressed → Triggering Fall + AddForce");
+                Character.localCharacter.GetType().GetMethod("Fall", BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic).Invoke(Character.localCharacter, new object[] { 1f });
+                Vector3 avarageVelocity = localCharacter.data.avarageVelocity;
+                if (avarageVelocity.magnitude < 0.1f)
+                {
+                    Debug.Log("Movement too small, no force applied.");
+                    return;
+                }
+                Vector3 vector = avarageVelocity.normalized * this.forceConfig.Value;
+                Character.localCharacter.GetType().GetMethod("AddForce", BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic).Invoke(Character.localCharacter, new object[] { vector, 1f, 1f });
+                Debug.Log(string.Format("Force applied: {0}", vector));
+            }
+        }
+    }
+}
